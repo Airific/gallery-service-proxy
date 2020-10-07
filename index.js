ed64f173b87fac1fd5f2cbbed662f88d4209eb0f
Victoria Chen <vicchen12@gmail.com>
2020-10-07 15:32:23 -0700

    Add request for each module

--- index.js
+++ index.js
@@ -2,33 +2,41 @@ const express = require('express');
 const path = require('path');
 const port = 3005;
 const app = express();
-// const { createProxyMiddleware} = require('http-proxy-middleware');
+const { createProxyMiddleware} = require('http-proxy-middleware');
 
 app.use('/:id', express.static(path.join(__dirname, './public')));
 
-// app.use(
-//   '/listings/gallery/:id',
-//   createProxyMiddleware({
-//     target: 'http://localhost:3001',
-//     changeOrigin: true,
-//   })
-// );
+app.use(
+  '/listings/gallery/:id',
+  createProxyMiddleware({
+    target: 'http://localhost:3001',
+    changeOrigin: true,
+  })
+);
 
-// app.use(
-//   '/listings/reviews/:id',
-//   createProxyMiddleware({
-//     target: 'http://localhost:3003',
-//     changeOrigin: true,
-//   })
-// );
+app.use(
+  '/listings/header/:id',
+  createProxyMiddleware({
+    target: 'http://localhost:3001',
+    changeOrigin: true,
+  })
+);
 
-// app.use(
-//   '/listings/moreplaces/:id',
-//   createProxyMiddleware({
-//     target: 'http://localhost:3002',
-//     changeOrigin: true,
-//   })
-// );
+app.use(
+  '/listings/reviews/:id',
+  createProxyMiddleware({
+    target: 'http://localhost:3003',
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  '/listings/moreplaces/:id',
+  createProxyMiddleware({
+    target: 'http://localhost:3002',
+    changeOrigin: true,
+  })
+);
 
 
 app.listen(port, () => {

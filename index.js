b728a8bfa329effc165d8d17e9129f30a3983215
Victoria Chen <vicchen12@gmail.com>
2020-10-07 11:20:38 -0700

    add bundle.js link from aws

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const express = require('express');
 const path = require('path');
 const port = 3005;
 const app = express();
+// const { createProxyMiddleware} = require('http-proxy-middleware');
 
 app.use('/:id', express.static(path.join(__dirname, './public')));
+
+// app.use(
+//   '/listings/gallery/:id',
+//   createProxyMiddleware({
+//     target: 'http://localhost:3001',
+//     changeOrigin: true,
+//   })
+// );
+
+// app.use(
+//   '/listings/reviews/:id',
+//   createProxyMiddleware({
+//     target: 'http://localhost:3003',
+//     changeOrigin: true,
+//   })
+// );
+
+// app.use(
+//   '/listings/moreplaces/:id',
+//   createProxyMiddleware({
+//     target: 'http://localhost:3002',
+//     changeOrigin: true,
+//   })
+// );
 
 
 app.listen(port, () => {
